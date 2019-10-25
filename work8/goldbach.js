d3b9071550c9e45d2db3104d695daad89fb55935
577-hjg <1437484617@qq.com>
2019-10-25 13:38:33 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,32 +1,38 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-        <script type="text/javascript">
-        function yueshu(a){
-            var count=0
-            for(var i=1; i<=a; i++){
-                if(a%i==0){
-                    count++
-                }
-            }
-            return count
+    //判断奇数偶数
+function goldbach(){
+    odd = document.getElementById('odd').value
+    a = parseInt(document.getElementById('odd').value);
+    if(isNaN(a)||a<=2||a%2==1){
+        alert('请输入一个大于2的偶数')
+        return false
+}else{
+    goldbach(odd)
+}
+//判断一个数是否为质数
+function wd(a){
+    var sum=0
+    for(var w=1;w<=a;w++){
+        if(a % w ==0){
+            sum++;
         }
-        function iszhishu(b){
-            if(yueshu(b)==2){
-                return true
-            }else{
-                return false
-            }
+    }
+    if(sum==2){
+        return true;
+    }else{
+        return false
+    }
+}
+//判断用户输入的偶数是否可分为俩个质数之和
+function goldbach(odd) {
+    let str=" ";
+    for(var i=2;i<odd;i++){
+        var j = odd - i;
+        if(wd(i)&&wd(j)&& i<=j){
+           str +=(odd+"可以拆分为俩个质数"+i+"与"+j+"的和"+'<br>');
         }
-        for(var i=4; i<=100; i+=2){
-            for(var j=2; j<i; j++){
-                if(iszhishu(j)&&iszhishu(i-j)){
-                    console.log(i+"可以拆分为"+j+"与"+(i-j))
-                }
-            }
-        }
-        </script>
-    </head>
-</html>
+    }
+
+    document.getElementById('goldbach').innerHTML = str
+
+}
+}
